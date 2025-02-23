85f943ce4160414139a9feeb5d1568b1e4c98a30
Diaa-Dw <oava9944@gmail.com>
2025-02-23 20:11:38 +0200

    Add overaly when navbar is open

--- index.js
+++ index.js
@@ -1,19 +1,24 @@
+//Header element
+const header = document.querySelector(".header");
 //Theme toggle button element
 const themeToggleBtn = document.querySelector(".theme-toggle-btn");
 const logo = document.querySelector(".logo");
 //Navbar elements
 const navbar = document.querySelector(".navbar");
 const navbarList = document.querySelector(".navbar-list");
-const sidebarBtn = document.querySelector(".burger-btn");
+const navbarOpenBtn = document.querySelector(".burger-btn");
 const navbarCloseBtn = document.querySelector(".navbar-close-btn");
+const overlay = document.querySelector(".overlay");
 
 //Function to handle open and close navbar
 const toggleNavbar = () => {
   navbar.classList.toggle("hidden");
+  overlay.classList.toggle("on");
+  document.body.classList.toggle("no-scroll");
 };
 
 //Event listeners to open and close navbar
-sidebarBtn.addEventListener("click", toggleNavbar);
+navbarOpenBtn.addEventListener("click", toggleNavbar);
 navbarCloseBtn.addEventListener("click", toggleNavbar);
 
 //Close navbar automaticly when navigate from one section to another
@@ -23,6 +28,28 @@ navbarList.addEventListener("click", (e) => {
   }
 });
 
+//function to show header if we scroll to top and hide it if we scroll to bottom
+let lastScrollTop = 0;
+window.addEventListener("scroll", () => {
+  let scrollTop = window.scrollY || document.documentElement.scrollTop;
+
+  if (scrollTop > lastScrollTop) {
+    header.style.transform = "translate(-50%,-100%)";
+  } else {
+    header.style.transform = "translate(-50%,0)";
+  }
+
+  lastScrollTop = scrollTop;
+});
+
+//function to close navbar if we click outside it
+document.addEventListener("click", (e) => {
+  if (!navbar.contains(e.target) && !navbarOpenBtn.contains(e.target)) {
+    navbar.classList.add("hidden");
+    overlay.classList.remove("on");
+    document.body.classList.remove("no-scroll");
+  }
+});
 
 themeToggleBtn.addEventListener("click", (e) => {
   e.preventDefault();
@@ -42,5 +69,6 @@ window.addEventListener("load", () => {
   const savedTheme = localStorage.getItem("theme");
   if (savedTheme) {
     document.documentElement.setAttribute("data-theme", savedTheme);
+    logo.setAttribute("src", `../assets/images/logo_${savedTheme}.svg`);
   }
 });
